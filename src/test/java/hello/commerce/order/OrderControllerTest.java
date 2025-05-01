c3a53c8d1f376899b22126602fe75da36888bfc7
yeonjue-2 <ilov-@naver.com>
2025-05-01 23:31:24 +0900

    fix: 기타 수정

--- src/test/java/hello/commerce/order/OrderControllerTest.java
+++ src/test/java/hello/commerce/order/OrderControllerTest.java
@@ -1,15 +1,12 @@
 package hello.commerce.order;
 
-import jdk.jshell.spi.ExecutionControl;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(OrderController.class)
 class OrderControllerTest {
